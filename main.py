25d7e96e9ce6f16eb1b9e9aecd312cb1e12cbbff
greg <gsc229@gmail.com>
2021-07-09 08:45:41 -0400

    removed select, data.gov has similar _fields parameter

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask, request
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from flask_cors import CORS
 from api import querySchools
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/school/<int:id>')
 def getSchoolById(id):
@@ -16,46 +17,25 @@ def getSchoolById(id):
   return { "data": data[0] }
 
 
-
 @app.route('/school')
 def getSchoolsByQuery():
-  """ GET request for schools by query with ability to select only certain data """
+  """ GET request for schools by query with ability to select only certain data nested up to four levels deep"""
   queryStr = request.query_string.decode("utf-8")
+
+  queryStr_has_select = "select" in queryStr
+
+  queryStr = queryStr.split("&")
   
-  if "select" in queryStr:
+  select = list(filter(lambda x: "select" in x, queryStr))[0] if queryStr_has_select else None
+  select = select.split("=")[1:][0].split(",") if queryStr_has_select else None
 
-    selectedData = []
-    
-    queryStr = queryStr.split("&")
-    
-    select = list(filter(lambda x: "select" in x, queryStr))[0]
-    select = select.split("=")[1:][0].split(",")
+  queryStr = list(filter(lambda x: "select" not in x, queryStr))
+  
+  queryStr = "&" + "&".join(queryStr)
 
-    queryStr = list(filter(lambda x: "select" not in x, queryStr))
-    
-    queryStr = "&" + "&".join(queryStr)
+  queryResult = querySchools(queryStr)
 
-    queryResult = querySchools(queryStr)
-    
-    if queryResult:
-      for school in queryResult["results"]:
-        modifiedResult = {}
-        modifiedResult["id"] = school["id"]
-        for field in select:
-          if field in school:
-            modifiedResult[field] = school[field]
-        selectedData.append(modifiedResult)
-    
-    return { "data": {"metadata": queryResult["metadata"], "results": selectedData} }
-  else:
-
-    queryResult = querySchools(queryStr)
-    return { "data": queryResult }
-    
-
-
-    
-
+  return { "data": queryResult }
 
 
 if __name__ == "__main__":
